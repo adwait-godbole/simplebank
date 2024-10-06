9854219dec24b5c7a355fa5f0880cff0888ac12f
Adwait Godbole <adwaitngodbole@gmail.com>
2024-10-06 10:22:48 +0530

    add JWT and PASETO token implementors and verifiers

---
+++ token/jwt_maker.go
@@ -0,0 +1,65 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const minSecretKeySize = 32
+
+// JWTMaker is a JSON Web Token maker
+type JWTMaker struct {
+	secretKey string
+}
+
+// CreateToken creates a new token for a specific username and time duration
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwtToken.SignedString([]byte(maker.secretKey))
+}
+
+// VerifyToken checks if the token is valid or not
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	jwtToken, err := jwt.ParseWithClaims(
+		token,
+		&Payload{},
+		// The below function checks whether the "alg" part of the JWT header is correct or not and no one has tampered with it by changing it to "none" or from "RSA" to "HS256"
+		func(token *jwt.Token) (interface{}, error) {
+			_, ok := token.Method.(*jwt.SigningMethodHMAC) // HS256 is an instance of the SigningMethodHMAC struct
+			if !ok {
+				return nil, ErrInvalidToken
+			}
+			return []byte(maker.secretKey), nil
+		},
+	)
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+}
+
+// NewJWTMaker creates a new JWTMaker
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
+	}
+	return &JWTMaker{secretKey: secretKey}, nil
+}
